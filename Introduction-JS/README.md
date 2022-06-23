6d021d930f33fdade2588abd2769ed68303513d0
Tijani1402 <99301758+Tijani1402@users.noreply.github.com>
2022-06-23 14:55:27 +0100

    updating all files

---
+++ Introduction-JS/README.md
@@ -0,0 +1,2 @@
+#javaScript
+<P>A JavaScript code that prints my name, height and country to the screen</p>
---
+++ Introduction-JS/script.js
@@ -0,0 +1,6 @@
+let MyName = "Tijani Ridwanlahi";
+let MyHeight = 1.79;
+let MyCountry = "Nigeria";
+
+//Printing my code to console
+console.log("Hello, My name is " + MyName + "from " + MyCountry + ". My height is " + MyHeight + "inches." )
